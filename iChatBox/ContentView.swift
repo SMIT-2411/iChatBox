ed06e6e7fb0e223de338a439129cc47d50073f05
Smit Patel <smit24@icloud.com>
2023-07-16 20:23:36 +0530

    Initial Commit

---
+++ iChatBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iChatBox
+//
+//  Created by Smit Patel on 16/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iChatBox/iChatBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatBoxApp.swift
+//  iChatBox
+//
+//  Created by Smit Patel on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iChatBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
